3539c9fcbc9c8e09fb01dac485dc0f6ea018ecf1
JKYangZY <tttok_jk@hotmail.com>
2017-07-03 12:22:25 +0800

    new

---
+++ TestFreamWork7/js/my-app.js
@@ -0,0 +1,18 @@
+//初始化Framework7
+var myApp = new Framework7({
+	modalTitle: '农业专家系统',
+	cache: true,
+	template7Pages: true,
+	material: true,
+	activeState: false,
+	pushState: false,
+	modalButtonOk: "确定",
+	modalButtonCancel: "取消",
+	//	tapHold: true,
+	init: true,
+	materialRipple: false
+	
+	
+});
+
+var $$ = Dom7;
